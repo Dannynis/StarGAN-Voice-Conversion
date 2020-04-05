421bf2732ab9f3c636679becb46a57fff6337062
SamuelBroughton <broughtonjsam@gmail.com>
2020-04-05 18:02:38 +0800

    star_cin_6

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ if __name__ == '__main__':
 
     # Model configuration.
     parser.add_argument('--num_speakers', type=int, default=4, help='dimension of speaker labels')
-    parser.add_argument('--lambda_cls', type=float, default=1, help='weight for domain classification loss')
+    parser.add_argument('--lambda_cls', type=float, default=10, help='weight for domain classification loss')
     parser.add_argument('--lambda_rec', type=float, default=10, help='weight for reconstruction loss')
-    parser.add_argument('--lambda_gp', type=float, default=5, help='weight for gradient penalty')
+    parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
     parser.add_argument('--lambda_id', type=float, default=5, help='weight for id mapping loss')
     parser.add_argument('--sampling_rate', type=int, default=16000, help='sampling rate')
 
